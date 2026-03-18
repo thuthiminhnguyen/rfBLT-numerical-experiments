a14561092c99a100bbb7d5fb88eb67f42b76a924
Thu Nguyen <61287645+thuthiminhnguyen@users.noreply.github.com>
2026-03-17 21:01:48 -0300

    Update code to save plots from Bayesian models

--- SmuEIR simulation - 10% noise level/training_period_102.R
+++ SmuEIR simulation - 10% noise level/training_period_102.R
@@ -99,7 +99,7 @@ abline(v=102, col="black", lty = 2)
 pred_size = 7
 window_size = 2*4+1
 
-# --- Model function ---
+# --- Random Feature Bayesian Lasso without Takens' theorem ---
 model_performance_lasso <- function(df,
                                     m,
                                     pred_size,
@@ -129,59 +129,30 @@ model_performance_lasso <- function(df,
                                         CI = 95)
   
   lasso_bayes_reg_model = ts_bayes$fit_results
+  lasso_bayes_fitted_values = ts_bayes$fitted.values
   lasso_posterior_samples = ts_bayes$posterior_samples
   lasso_bayes_future_preds = ts_bayes$future_preds  # matrix
   lasso_bayes_y_pred = ts_bayes$y_pred              # vector
   lasso_bayes.pred.ci = ts_bayes$pred.ci
   lasso_bayes.pred.lower.ci = lasso_bayes.pred.ci[,1]
   lasso_bayes.pred.upper.ci = lasso_bayes.pred.ci[,2]
-  lasso_ess_plot = ts_bayes$ess_plot  
-  
-  ts_bayes_taken <- ts_forecast_bayes_reg_rfm_taken(
-      ts_data = as.vector(df[1:m]),
-      time = 1:m,
-      smooth_diff = FALSE,
-      window_size = window_size,
-      pred_size = pred_size,
-      weight_dist = "normal",
-      weight_params = list(),
-      bias_dist = "uniform",
-      bias_params = list(min_val = 0, max_val = 2*pi),
-      act_func = "fourier",
-      reg_type = "lasso",
-      burnin = 1000,
-      n.samples = 2000,
-      thin = 5,
-      n.cores = NULL,
-      pred_type = "mean",
-      feature_selection = feature_selection,
-      feature_constant = feature_constant,
-      CI = 95
-    ) 
-  
-  # Model output
-  lasso_taken_bayes_reg_model = ts_bayes_taken$fit_results
-  lasso_taken_posterior_samples = ts_bayes_taken$posterior_samples
-  
-  lasso_taken_bayes_future_y_preds = ts_bayes_taken$future_y_preds             
-  lasso_taken_bayes_y_pred = ts_bayes_taken$y_pred                             
-  lasso_taken_bayes.pred.ci = ts_bayes_taken$pred.ci
-  lasso_taken_bayes.pred.lower.CI = lasso_taken_bayes.pred.ci[,1]
-  lasso_taken_bayes.pred.upper.CI = lasso_taken_bayes.pred.ci[,2]
-  
-  lasso_taken_ess_plot = ts_bayes_taken$ess_plot
-  
-  return(list(y_true = y_true,
+  lasso_ess_plot = ts_bayes$ess  
+  lasso_coef <- ts_bayes$posterior_samples
+  lasso_sigma2 <- ts_bayes$posterior_sigma2
+    
+  error_df <- data.frame(
+    bayes_lasso = vec_relative_error(matrix(lasso_bayes_y_pred, nrow = 1, byrow = TRUE), y_true)
+  )
+  return(list(error_df = error_df,
+              y_true = y_true,
               bayes_lasso_fit = list(fit = lasso_bayes_reg_model,
+                                     fitted_values = lasso_bayes_fitted_values,
                                      pred = lasso_bayes_y_pred,
+                                     coef = lasso_coef,
+                                     sigma2 = lasso_sigma2,
                                      lowerCI = lasso_bayes.pred.lower.ci,
                                      upperCI = lasso_bayes.pred.upper.ci,
-                                     ess = lasso_ess_plot),
-              bayes_lasso_taken_fit = list(fit = lasso_taken_bayes_reg_model,
-                                           pred = lasso_taken_bayes_y_pred,
-                                           lowerCI = lasso_taken_bayes.pred.lower.CI,
-                                           upperCI = lasso_taken_bayes.pred.upper.CI,
-                                           ess = lasso_taken_ess_plot)))
+                                     ess = lasso_ess_plot)))
 }
 
 # --- Create empty dfs to save results ---
@@ -193,45 +164,106 @@ bayes_lasso_lowerCI <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size,
 bayes_lasso_upperCI <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE)
 bayes_lasso_y_pred <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE)
 
-# Bayesian Lasso Taken without Smoothness
-bayes_lasso_taken_lowerCI <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE)
-bayes_lasso_taken_upperCI <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE) 
-bayes_lasso_taken_y_pred <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE) 
-
 # Bayesian Lasso Taken with Smoothness
 bayes_lasso_taken_normal_lowerCI <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE)
 bayes_lasso_taken_normal_upperCI <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE) 
 bayes_lasso_taken_normal_y_pred <- matrix(rep(NA, (n_simulation*pred_size)), ncol=pred_size, nrow=n_simulation, byrow=TRUE) 
 
-# --- rfBL, rfBLT with and without smoothness derivatives ---
+# --- RF, rfBLT ---
 fixed_point = 102
 
+# --- RF ---
+bayes_lasso_ess_list <- list()
+bayes_lasso_coef_list <- list()
+bayes_lasso_sigma2_list <- list()
+bayes_lasso_residual_trend_train_mat <- matrix(NA, nrow=n_simulation, ncol=(fixed_point-window_size))
+df_residuals_acf_rfBL <- matrix(NA, nrow=n_simulation, ncol=(fixed_point-window_size))
+
 for (i in 1:n_simulation){
+  print(i)
   data_vec = as.vector(I_noise_avg_mat[i,])
+  # smoothed_vec = as.vector(fwd_diff_mat[i,])
   res <- model_performance_lasso(
     df = data_vec,
+    # smoothed = smoothed_vec,
     m = fixed_point,
     pred_size = pred_size,
     window_size = window_size,
     feature_selection = "factor",
     feature_constant = 0.5
   )
+  # True value
+  y_true_mat[i,] = as.vector(res$y_true)
+
+  # Model residuals
+  train_mat <- embed(data_vec[1:(fixed_point)], (window_size+1))
+  residuals <- train_mat[,1] - res$bayes_lasso_fit$fitted_values
+  bayes_lasso_residual_trend_train_mat[i, 1:length(residuals)] <- residuals
+
+  residual_acf <- acf(residuals, lag.max=fixed_point-window_size+1, plot=FALSE)
+  df_residuals_acf_rfBL[i, ] <- as.vector(residual_acf$acf) 
   
   # Bayesian Lasso
   bayes_lasso_lowerCI[i,] = as.vector(res$bayes_lasso_fit$lowerCI)
   bayes_lasso_upperCI[i,] = as.vector(res$bayes_lasso_fit$upperCI)
   bayes_lasso_y_pred[i,] = as.vector(res$bayes_lasso_fit$pred)
-  
-  # Bayesian Lasso Taken
-  bayes_lasso_taken_lowerCI[i,] = as.vector(res$bayes_lasso_taken_fit$lowerCI)
-  bayes_lasso_taken_upperCI[i,] = as.vector(res$bayes_lasso_taken_fit$upperCI)
-  bayes_lasso_taken_y_pred[i,] = as.vector(res$bayes_lasso_taken_fit$pred)
+    
+  bayes_lasso_ess_list[[i]] <- res$bayes_lasso_fit$ess
+  bayes_lasso_coef_list[[i]] <- res$bayes_lasso_fit$coef
+  bayes_lasso_sigma2_list[[i]] <- res$bayes_lasso_fit$sigma2
 }
 
+# Save results for RF
+## ESS plots
+pdf("train102_RF_ess.pdf", width=7, height=5)
+par(mfrow=c(1, 1))
 for (i in 1:n_simulation){
+    plot(bayes_lasso_ess_list[[i]])
+}
+dev.off()
+
+## Residuals
+pdf("train102_RF_residuals.pdf", width=10, height=4)
+par(mfrow=c(1, 2))
+for (i in 1:dim(df_residuals_acf_rfBL)[1]){
+  # ACF of residuals
+  plot(df_residuals_acf_rfBL[i, ], type="h", ylab="ACF", xlab="Lag", main=paste(i))
+  # QQ plot of residuals
+  qqnorm(na.omit(bayes_lasso_residual_trend_train_mat[i,]), main=paste(i))
+  qqline(na.omit(bayes_lasso_residual_trend_train_mat[i,]))
+}
+dev.off()
+
+## Coefficients of last iter
+pdf("train102_RF_coefs_samples.pdf", width=15, height=20)
+par(mfrow=c(6, 3))
+for (i in 1:ncol(res$bayes_lasso_fit$coef)){
+  plot(res$bayes_lasso_fit$coef[,i], type="l", main=paste0("Tract plot of Beta ", i-1), ylab=paste0("Beta ", i-1))
+  hist(res$bayes_lasso_fit$coef[,i], probability = TRUE, main=paste0("Density of Beta ", i-1), xlab=paste0("Beta ", i-1))
+  boxplot(res$bayes_lasso_fit$coef[,i], horizontal=TRUE, main=paste0("Box plot of Beta ", i-1))
+}
+dev.off()
+
+## Sigma2 of last iter
+pdf("train102_RF_sigma2_samples.pdf", width=15, height=4)
+par(mfrow=c(1, 3))
+plot(res$bayes_lasso_fit$sigma2, type="l", ylab="Sigma2", main="Trace plot of Sigma2")
+hist(res$bayes_lasso_fit$sigma2, probability = TRUE, main=paste0("Density of Sigma2"), xlab=paste0("Sigma2"))
+boxplot(res$bayes_lasso_fit$sigma2, horizontal=TRUE, main="Box plot of Sigma2")
+dev.off()
+
+# --- rfBLT ---
+bayes_lasso_taken_normal_ess_list <- list()
+bayes_lasso_taken_normal_coef_list <- list()
+bayes_lasso_taken_normal_sigma2_list <- list()
+bayes_lasso_taken_residual_trend_train_mat <- matrix(NA, nrow=n_simulation, ncol=(fixed_point-window_size))
+df_residuals_acf_rfBLT <- matrix(NA, nrow=n_simulation, ncol=(fixed_point-window_size))
+
+for (i in 1:n_simulation){
+  print(i)
   data_vec = as.vector(I_noise_avg_mat[i,])
   input_vec = data_vec[1:fixed_point]
-  
+  # smoothed_vec = as.vector(smoothed_mat[i,])
   res <- ts_forecast_bayes_reg_rfm_taken(
       ts_data = input_vec,
       time = 1:fixed_point,
@@ -254,13 +286,64 @@ for (i in 1:n_simulation){
       feature_selection = "factor",
       feature_constant = 0.5,
       CI = 95
-    ) 
+    )
+
+  # Residuals
+  train_mat <- embed(data_vec[1:(fixed_point)], (window_size+1))
+  residuals <- train_mat[,1] - res$fitted.values
+  bayes_lasso_taken_residual_trend_train_mat[i, 1:length(residuals)] <- residuals
+
+  residual_acf <- acf(residuals, lag.max=fixed_point-window_size+1, plot=FALSE)
+  df_residuals_acf_rfBLT[i, ] <- as.vector(residual_acf$acf) 
   
   # Prediction  
   bayes_lasso_taken_normal_lowerCI[i,] = as.vector(res$pred.ci[,1])
   bayes_lasso_taken_normal_upperCI[i,] = as.vector(res$pred.ci[,2])
   bayes_lasso_taken_normal_y_pred[i,] = as.vector(res$y_pred)
+
+  bayes_lasso_taken_normal_ess_list[[i]] <- res$ess
+  bayes_lasso_taken_normal_coef_list[[i]] <- res$posterior_samples
+  bayes_lasso_taken_normal_sigma2_list[[i]] <- res$posterior_sigma2
 }
+
+# Save results for rfBLT
+## ESS plots
+pdf("train102_rfBLT_ess.pdf", width=7, height=5)
+par(mfrow=c(1, 1))
+for (i in 1:n_simulation){
+    plot(bayes_lasso_taken_normal_ess_list[[i]])
+}
+dev.off()
+
+## Residuals
+pdf("train102_rfBLT_residuals.pdf", width=10, height=4)
+par(mfrow=c(1, 2))
+for (i in 1:dim(df_residuals_acf_rfBLT)[1]){
+  # ACF of residuals
+  plot(df_residuals_acf_rfBLT[i, ], type="h", ylab="ACF", xlab="Lag", main=paste(i))
+  # QQ plot of residuals
+  qqnorm(na.omit(bayes_lasso_taken_residual_trend_train_mat[i,]), main=paste(i))
+  qqline(na.omit(bayes_lasso_taken_residual_trend_train_mat[i,]))
+}
+dev.off()
+
+## Coefficients of last iter
+pdf("train102_rfBLT_coefs_samples.pdf", width=15, height=20)
+par(mfrow=c(6, 3))
+for (i in 1:ncol(res$posterior_samples)){
+  plot(res$posterior_samples[,i], type="l", main=paste0("Tract plot of Beta ", i-1), ylab=paste0("Beta ", i-1))
+  hist(res$posterior_samples[,i], probability = TRUE, main=paste0("Density of Beta ", i-1), xlab=paste0("Beta ", i-1))
+  boxplot(res$posterior_samples[,i], horizontal=TRUE, main=paste0("Box plot of Beta ", i-1))
+}
+dev.off()
+
+## Sigma2 of last iter
+pdf("train102_rfBLT_sigma2_samples.pdf", width=15, height=4)
+par(mfrow=c(1, 3))
+plot(res$posterior_sigma2, type="l", ylab="Sigma2", main="Trace plot of Sigma2")
+hist(res$posterior_sigma2, probability = TRUE, main=paste0("Density of Sigma2"), xlab=paste0("Sigma2"))
+boxplot(res$posterior_sigma2, horizontal=TRUE, main="Box plot of Sigma2")
+dev.off()
 
 # True matrix
 y_true_mat = I_noise_avg_mat[,(fixed_point+1):(fixed_point+pred_size)]
@@ -269,11 +352,6 @@ y_true_mat = I_noise_avg_mat[,(fixed_point+1):(fixed_point+pred_size)]
 m102_pred_bayes_lasso = bayes_lasso_y_pred
 m102_lowerCI_bayes_lasso = bayes_lasso_lowerCI
 m102_upperCI_bayes_lasso = bayes_lasso_upperCI
-
-# Bayes Lasso Takens without Smoothing Derivatives
-m102_pred_bayes_lasso_taken = bayes_lasso_taken_y_pred
-m102_lowerCI_bayes_lasso_taken = bayes_lasso_taken_lowerCI
-m102_upperCI_bayes_lasso_taken = bayes_lasso_taken_upperCI
 
 # Bayes Lasso Takens with Smoothing Derivatives
 m102_pred_BLT_normal_error <- bayes_lasso_taken_normal_y_pred
@@ -339,11 +417,6 @@ save(I_noise_avg_mat,
      m102_pred_bayes_lasso,
      m102_lowerCI_bayes_lasso,
      m102_upperCI_bayes_lasso,
-     
-     # rfBLT without smoothness
-     m102_pred_bayes_lasso_taken,
-     m102_lowerCI_bayes_lasso_taken,
-     m102_upperCI_bayes_lasso_taken,
 
      # rfBLT with smoothness
      m102_pred_BLT_normal_error,
